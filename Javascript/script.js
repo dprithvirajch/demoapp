cc9188ad05df651bdf09e5e1d066469024c5cada
dprithvirajch <prithvi30@gmail.com>
2022-12-13 16:24:53 +0530

    pxinit

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,9 +1,29 @@
+
+  (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-E3AJGZV92KYS-2");
+
+
 function allowlogin(usermail)
 {
     var a = document.getElementById("usermail").value;
     if (a == 'demouser@gmail.com')
     {
         alert('valid user');
+
+        aptrinsic("identify",
+            {
+
+                "id": "GS1234",
+                "email": a
+            },
+            {
+                "id": "GS",
+                "name": "Gainsight"
+            });
+
         location.href = "Page1.html";
     }
     else if(a == '')
